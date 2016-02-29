325f1e9d8dc8175c5a690b213ba12d35dfcf680f
musaichi <musaichich@gmail.com>
2016-02-29 16:18:54 +0900

    残りTask作成のみ

--- src/Tips/Tips.php
+++ src/Tips/Tips.php
@@ -4,19 +4,35 @@ namespace Tips;
 
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\Player;
 use pocketmine\utils\Config;
+use pocketmine\scheduler\PluginTask;
 
 class Tips extends pluginBase implements Listener{
-    public function onEnable() {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        if (!file_exists($this->getDataFolder())) @mkdir($this->getDataFolder(), 0755, true);
-        $this->message = new Config($this->getDataFolder()."message.yml", Config::YAML, array(
+
+ function onEnable(){
+  $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  if (!file_exists($this->getDataFolder())) @mkdir($this->getDataFolder(), 0755, true);
+  $this->message = new Config($this->getDataFolder()."message.yml", Config::YAML, array(
             "1" => 'ﾃﾞﾓﾍﾟﾁﾉｶﾞﾓｯﾄｶﾜｲｲﾖｰ',
             "2" => 'ｳｻｷﾞｶﾞﾆｹﾞﾃﾙ!',
             "3" => 'にゃんぱすー',
             "4" => 'コプァ...',
             "5" => 'そすんさー',
             ));
-        $this->country->save();
-    }
+  $this->setup = new Config($this->getDataFolder()."setup.yml", Config::YAML, array(
+            "time" => '60',
+            ));
+  $this->setup->save();
+  $this->country->save();
+ }
+
+ function onRun(){
+  $c = $this->message;
+  $s = $this->setup->get('time');
+  $a = $s * 20;
+  $t = new MessageTask($this, $c);
+  $this->getServer()->getScheduler()->scheduleDeleyedTask($t, $a);
+ }
 }
